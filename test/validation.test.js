27a99a5908d2d5095a65c706bf5e3c3b4980c2bc
laura.souza <laura.souza@gclaims.com.br>
2023-04-19 00:57:52 -0300

    tests

--- test/validation.test.js
+++ test/validation.test.js
@@ -19,3 +19,36 @@ it('Caso inválido - sem o parâmetro nome', () => {
     });
     expect(result.name).toEqual(Constants.ErrorValidation.name);
 });
+
+it('Caso válido', () => {
+    const result = Validation.update({
+        nome: "Gato de botas",
+        produtora: "Universal Pictures",
+        ano: "2023",
+        imdb: "8.5"
+    });
+    expect(result).toEqual(undefined);
+});
+
+it('Caso inválido - sem algum parâmetro', () => {
+    const result = Validation.update({
+        nome: "Gato de botas",
+        ano: "2023",
+        imdb: "8.5"
+    });
+    expect(result.name).toEqual(Constants.ErrorValidation.name);
+});
+
+it('Caso inválido - filme não encontrado', () => {
+    const result = Validation.delete({
+        
+    });
+    expect(result.name).toEqual(Constants.ErrorValidation.name);
+});
+
+it('Caso válido', () => {
+    const result = Validation.delete({
+        nome: "Gato de botas",
+    });
+    expect(result).toEqual(undefined);
+});
